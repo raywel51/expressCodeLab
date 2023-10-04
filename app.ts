8c81acf27f0e793cfe60e1bb774ddad568783b30
nonthawat <kittayawat.aong@gmail.com>
2023-10-04 21:53:09 +0700

    add ecosystem.config.json file random time to genarate

--- app.ts
+++ app.ts
@@ -1,21 +1,23 @@
 import { generateThaiLicensePlate } from './src/utils/generateThaiLicensePlate';
 import {connectToRabbitMQ} from "./src/connection/mqtt"; // Import the function
 
-function loopWithDelay() {
-    const delay: number = 30000; // Delay in milliseconds (adjust as needed)
+async function main() {
+    const minValue: number = 2000;
+    const maxValue: number = 60000;
+
+    const randomDelay: number = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 
     async function iteration() {
         const licensePlate: string = generateThaiLicensePlate();
+        console.log(licensePlate);
         await connectToRabbitMQ(licensePlate)
 
-        setTimeout(iteration, delay);
+        console.log("next data in "+randomDelay / 1000 + "s")
+        setTimeout(iteration, randomDelay);
     }
 
-    // Start the loop
-    iteration().then(r =>
-        console.log("Started")
-    )
+
+    await iteration()
 }
 
-// Call the loopWithDelay function to start the loop
-loopWithDelay();
+main()
