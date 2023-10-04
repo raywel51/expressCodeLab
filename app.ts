8ab0bf5da3be9c6cbec629d6c1845d8c1fbfe3eb
nonthawat <kittayawat.aong@gmail.com>
2023-10-04 21:59:28 +0700

    fix app.ts move randomDelay to iteration()

--- app.ts
+++ app.ts
@@ -5,13 +5,12 @@ async function main() {
     const minValue: number = 2000;
     const maxValue: number = 60000;
 
-    const randomDelay: number = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-
     async function iteration() {
         const licensePlate: string = generateThaiLicensePlate();
         console.log(licensePlate);
         await connectToRabbitMQ(licensePlate)
 
+        const randomDelay: number = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
         console.log("next data in "+randomDelay / 1000 + "s")
         setTimeout(iteration, randomDelay);
     }
